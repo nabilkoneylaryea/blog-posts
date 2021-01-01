28512fdc73410aa46a0b5e19728cf1c5aaec5379
Nabil <nkoney14@outlook.com>
2021-01-01 00:25:52 -0500

    built out basic functionality using sample data in js file rendered with exphbs

---
+++ BlogPosts.js
@@ -0,0 +1,26 @@
+//SAMPLE BLOG POSTS
+const blogPosts = [
+    {
+        id: 1,
+        title: 'Blog Post 1',
+        author: 'Nabil Koney-Laryea',
+        time: Date(),
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem consequatur hic vitae facere sed asperiores error nemo esse consequuntur delectus optio dicta itaque vero dolorum rerum, voluptate temporibus, totam repellendus!'
+    },
+    {
+        id: 2,
+        title: 'Blog Post 2',
+        author: 'Nabil Koney-Laryea',
+        time: Date(),
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem consequatur hic vitae facere sed asperiores error nemo esse consequuntur delectus optio dicta itaque vero dolorum rerum, voluptate temporibus, totam repellendus!'
+    },
+    {
+        id: 3,
+        title: 'Blog Post 3',
+        author: 'Nabil Koney-Laryea',
+        time: Date(),
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem consequatur hic vitae facere sed asperiores error nemo esse consequuntur delectus optio dicta itaque vero dolorum rerum, voluptate temporibus, totam repellendus!'
+    },
+];
+
+module.exports = blogPosts;
---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const mongoose = require('mongoose');
+const blogPosts = require('./BlogPosts');
+
+// INIT APP
+const app = express();
+
+// INIT ENGINE
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// SET STATIC FOLDER
+app.use(express.static(path.join(__dirname, 'public')));
+
+// SET UP BASIC ROUTE TO MAKE SURE EVERYTHING IS WORKING
+app.get('/', (req, res) => {
+    res.render('index', {posts: blogPosts});
+});
+
+// MUST LISTEN ON PORT
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT} ...`);
+});
